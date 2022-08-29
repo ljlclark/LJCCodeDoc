6d2c9a45a1961f1b873dd5b3888a90d403d66ae9
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-08-30 05:01:24 +0800

    Change html

--- Script/CCodeDocNavEvents.js
+++ Script/CCodeDocNavEvents.js
@@ -15,23 +15,84 @@ class CNavEvents
   // Adds the HTML event handlers.
   AddEvents()
   {
-    // Document Event Handlers.
+    // Event Handlers.
+    window.addEventListener("resize", this.WindowResize.bind(this))
     document.addEventListener("click", this.DocumentClick.bind(this));
+    menuIco.addEventListener("click", this.MenuClick.bind(this));
+    content.addEventListener("mouseenter", this.ContentMouseEnter.bind(this));
+
+    this.menubar = document.getElementById("menubar");
+    this.menuIco = document.getElementById("menuIco");
+    this.sidebar = document.getElementById("sidebar");
+    this.content = document.getElementById("content");
+    this.menubar.style.display = "none";
+    this.menuIco.style.display = "none";
+    this.reducedWidth = false;
+    this.WindowResize();
+  }
+
+  // 
+  WindowResize()
+  {
+    let width = window.innerWidth;  // Webpage with scrollbars
+    if (width < 800)
+    {
+      this.reducedWidth = true;
+      this.menubar.style.display = "block";
+      this.menuIco.style.display = "block";
+      this.sidebar.style.display = "none";
+      this.sidebar.style.width = "240px";  // use widest string width?
+      this.content.style.width = "100%";
+    }
+    else
+    {
+      this.reducedWidth = false;
+      this.menubar.style.display = "none";
+      this.menuIco.style.display = "none";
+      this.sidebar.style.display = "inline-block";
+      this.sidebar.style.position = "relative";
+      this.sidebar.style.width = "25%";  // use widest string width?
+      this.content.style.width = "75%";
+    }
+  }
+
+  // 
+  MenuClick()
+  {
+    if (this.sidebar.style.display == "none")
+    {
+      this.sidebar.style.display = "inline-block";
+      this.sidebar.style.position = "absolute";
+    }
+    else
+    {
+      this.sidebar.style.display = "none";
+    }
+  }
+
+  // 
+  ContentMouseEnter()
+  {
+    if (this.reducedWidth == true)
+    {
+      this.sidebar.style.display = "none";
+    }
   }
 
   // Document "click" handler method.
   // event - The Target event.
   DocumentClick(event)
   {
-    const srcElement = event.target;
+    let srcElement = event.target;
     if ("navGroup" === srcElement.className
       || "navItem" === srcElement.className)
     {
-      const navItem = this.NavItems.SearchName(srcElement.id);
+      let navItem = this.NavItems.SearchName(srcElement.id);
       if (navItem != null)
       {
         if (this.ContentFrame != null)
         {
+          this.ContentMouseEnter();
           this.ContentFrame.src = navItem.URL;
         }
       }
