27ad52de94b91a53a4b7e3fc3e8c7ba4a4c27cca
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-11-16 03:54:00 +0800

    Update Pages

--- HTML/LJCNetCommon/AfterGenCopyCustom.cmd
+++ HTML/LJCNetCommon/AfterGenCopyCustom.cmd
@@ -1,20 +1,20 @@
 
 xcopy ..\..\..\..\..\WebPages\LJCCodeDoc\HTML\LJCNetCommon\*.* ..\LJCNetCommonCompare /s /y
 
-ren DbColumns.html DbColumnsGen.html
+copy DbColumns.html DbColumnsGen.html
 copy DbColumnsCustom.html DbColumns.html
 
-ren DbValues.html DbValuesGen.html
+copy DbValues.html DbValuesGen.html
 copy DbValuesCustom.html DbValues.html
 
-ren KeyItems.html KeyItemsGen.html
+copy KeyItems.html KeyItemsGen.html
 copy KeyItemsCustom.html KeyItems.html
 
-ren LJCAssemblyReflect.html LJCAssemblyReflectGen.html
+copy LJCAssemblyReflect.html LJCAssemblyReflectGen.html
 copy LJCAssemblyReflectCustom.html LJCAssemblyReflect.html
 
-ren NetCommon.html NetCommonGen.html
+copy NetCommon.html NetCommonGen.html
 copy NetCommonCustom.html NetCommon.html
 
-ren NetString.html NetStringGen.html
+copy NetString.html NetStringGen.html
 copy NetStringCustom.html NetString.html
